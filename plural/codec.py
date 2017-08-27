65a4ad8ca144efdde66aa5add91083a496585f3e
Gabriel Falcão <gabriel@nacaolivre.org>
2017-08-27 01:32:29 -0400

    binary and compressed codecs

--- plural/codec.py
+++ plural/codec.py
@@ -15,76 +15,91 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import bz2
 import json
+import zlib
 from decimal import Decimal
 from dateutil.parser import parse as parse_datetime
 
 
 class Codec(object):  # pragma: no cover
-    def decode(self, string):
-        raise NotImplementedError
-
-    def encode(self, string):
-        raise NotImplementedError
-
-
-class Unicode(Codec):
-    """codec for UTF-8 encoded strings"""
-    encoding = 'utf-8'
-
-    def encode(self, string):
-        if not string:
+    def encode(self, data):
+        if not data:
             return ''
 
-        return string.encode(self.encoding)
+        return self.dumps(data)
 
     def decode(self, string):
         if not isinstance(string, basestring):
             return string
 
+        return self.loads(string)
+
+    def loads(self, string):
+        raise NotImplementedError
+
+    def dumps(self, string):
+        raise NotImplementedError
+
+
+class Binary(Codec):
+    def loads(self, binary):
+        return binary
+
+    def dumps(self, binary):
+        return binary
+
+
+class Gzip(Codec):
+    def loads(self, binary):
+        return zlib.deompress(binary)
+
+    def dumps(self, binary):
+        return zlib.compress(binary)
+
+
+class Bzip2(Codec):
+    def loads(self, binary):
+        return bz2.deompress(binary)
+
+    def dumps(self, binary):
+        return bz2.compress(binary)
+
+
+class Unicode(Codec):
+    """codec for UTF-8 dumpsd strings"""
+    encoding = 'utf-8'
+
+    def dumps(self, string):
+        return string.encode(self.encoding)
+
+    def loads(self, string):
         return string.decode(self.encoding)
 
 
 class DateTime(Codec):
     """codec for datetime objects"""
-    def encode(self, datetime):
-        if not datetime:
-            return ''
-
+    def dumps(self, datetime):
         return datetime.isoformat()
 
-    def decode(self, string):
-        if not isinstance(string, basestring):
-            return string
-
+    def loads(self, string):
         return parse_datetime(string)
 
 
 class Number(Codec):
     """codec for Decimal objects"""
-    def encode(self, number):
-        if not number:
-            return ''
+    def dumps(self, number):
         return bytes(number)
 
-    def decode(self, string):
-        if not isinstance(string, basestring):
-            return string
-
+    def loads(self, string):
         return Decimal(string)
 
 
 class JSON(Codec):
     """codec for JSON objects"""
 
-    def encode(self, data):
-        if not data:
-            return ''
-
+    def dumps(self, data):
         return json.dumps(data)
 
-    def decode(self, string):
-        if not isinstance(string, basestring):
-            return string
-
+    def loads(self, string):
         return json.loads(string)
